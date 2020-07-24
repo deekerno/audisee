0072045c6dfde55f7de30bc74538a9dcb01c909e
adcrn <9167686+adcrn@users.noreply.github.com>
2020-07-23 20:29:15 -0400

    Added spectrogram generation

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,7 @@ use rustfft::FFTplanner;
 /// use of the frequencies calculated in the spectrum as well as their
 /// respective magnitudes.
 pub fn amp_spectrum(signal: &[f64]) -> Vec<f64> {
+    // Apply a Hanning window in order to smooth things out.
     let windowed_signal = apply_window(signal);
 
     let spectrum = fft(&windowed_signal[..]);
@@ -53,6 +54,66 @@ pub fn amp_spectrum(signal: &[f64]) -> Vec<f64> {
 /// between frequency bins.
 pub fn power_spectrum(amp_spectrum: &[f64]) -> Vec<f64> {
     amp_spectrum.iter().map(|bin| bin.powi(2)).collect()
+}
+
+/// Returns the amplitude spectrum of an entire signal through time.
+///
+/// This function takes the input signal (an audio time-series) and pads it before generating
+/// overlapping frames. Each overlapping frame is windowed using the Hann function and then
+/// processed through FFT. An amplitude (magnitude) spectrum is created for each of the resultant
+/// complex spectrums. The output is a vector of amplitude spectrums for each overlapping frame.
+pub fn spectrogram(signal: &[f64]) -> Vec<Vec<f64>> {
+    // Pad the signal before calculating the FFT.
+    let padding_needed = signal.len() % 4;
+    let padded_signal = if padding_needed != 0 {
+        let mut signal_clone = signal.to_vec().clone();
+        let mut padding = vec![0_f64; padding_needed];
+        signal_clone.append(&mut padding);
+        signal_clone
+    } else {
+        signal.to_vec().clone()
+    };
+
+    // Apply window to the padded signal to prevent jankiness.
+    // Then, generate overlapping frames for the signal.
+    let frame_size = padded_signal.len() / 4;
+    let overlapping_frames = utils::OverlappingFrames::new(&padded_signal, frame_size, 0.75);
+
+    // Re-use FFT planner as recommended in RustFFT docs.
+    let mut planner = FFTplanner::new(false);
+
+    // Reduce likelihood of decreased performance due to constant allocation
+    // by pre-allocating a vector to a capacity equal to the amount of frames.
+    let (_, upper_bound) = overlapping_frames.size_hint();
+    let capacity = match upper_bound {
+        Some(n) => n,
+        None => frame_size * 2,
+    };
+    let mut amp_spec_frames = Vec::with_capacity(capacity);
+
+    for frame in overlapping_frames {
+        let fft = planner.plan_fft(frame.len());
+        let windowed_frame = apply_window(&frame[..]);
+
+        let mut complex_frame = windowed_frame
+            .iter()
+            .map(|x| Complex::new(*x, 0f64))
+            .collect::<Vec<Complex<f64>>>();
+
+        let mut spectrum = vec![Complex::new(0_f64, 0_f64); complex_frame.len()];
+        fft.process(&mut complex_frame, &mut spectrum);
+
+        // Create magnitude spectrum for the frame.
+        let spec_frame = spectrum
+            .iter()
+            .take(spectrum.len() / 2)
+            .map(|bin| (bin.re.powi(2) + bin.im.powi(2)).sqrt())
+            .collect::<Vec<f64>>();
+
+        amp_spec_frames.push(spec_frame);
+    }
+
+    amp_spec_frames
 }
 
 /// Returns the output of the fast Fourier transform using a signal as input.
